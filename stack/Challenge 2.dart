4fb402ce1943af98b41f9e6d9311b82b63f3314a
yara jehad rabaya 10 <122912660+yarajehadrabaya@users.noreply.github.com>
2023-12-21 23:29:08 +0300

    Challenge 2.dart

--- stack/Challenge 2.dart
+++ stack/Challenge 2.dart
@@ -1,3 +1,13 @@
+class Stack<E> {
+ Stack() : _storage = <E>[];
+ final List<E> _storage;
+  void push(E element) => _storage.add(element);
+  E pop() => _storage.removeLast();
+  E get peek => _storage.last;
+  bool get isEmpty => _storage.isEmpty;
+  bool get isNotEmpty => !isEmpty;
+}
+
 void main() {
   String test1 = "(((()(1)()(0))))"; // Balanced parentheses
   String test2 = "((1))))))"; // Unbalanced parentheses
@@ -10,26 +20,20 @@ void main() {
 }
 
 bool checkParentheses(String text) {
-  var stack = <int>[];
-  final open = '('.codeUnitAt(0);
-  final close = ')'.codeUnitAt(0);
-
-  for (int codeUnit in text.codeUnits) {
-    if (codeUnit == open) {
-      stack.add(codeUnit);
-    } else if (codeUnit == close) {
-      if (stack.isEmpty) {
-        return false;
-      } else {
-        if (stack.last == open) {
-          stack.removeLast();
-        } else {
-          return false;
-        }
-      }
-    }
-  }
-
-  return stack.isEmpty;
+ final stack = Stack<int>();
+ final open = '('.codeUnitAt(0);
+ final close = ')'.codeUnitAt(0);
+ for (int codeUnit in text.codeUnits) {
+ if (codeUnit == open) {
+ stack.push(codeUnit);
+ } else if (codeUnit == close) {
+ if (stack.isEmpty) {
+ return false;
+ } else {
+ stack.pop();
+ }
+ }
+ }
+ return stack.isEmpty;
 }
 
