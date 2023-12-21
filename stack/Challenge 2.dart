c3a4edd628a63e48dd969d810e9d3999b389a4fa
yara jehad rabaya 10 <122912660+yarajehadrabaya@users.noreply.github.com>
2023-12-21 22:55:43 +0300

    Challenge 2.dart

--- stack/Challenge 2.dart
+++ stack/Challenge 2.dart
@@ -1,34 +1,35 @@
-import 'dart:io';
-
 void main() {
   String test1 = "(((()(1)()(0))))"; // Balanced parentheses
   String test2 = "((1))))))"; // Unbalanced parentheses
 
   print("String: $test1");
-  print("is Balanced? ${check_balanced(test1)}");
+  print("is Balanced? ${checkParentheses(test1)}");
 
   print("\nString: $test2");
-  print("is Balanced? ${check_balanced(test2)}");
+  print("is Balanced? ${checkParentheses(test2)}");
 }
 
+bool checkParentheses(String text) {
+  var stack = <int>[];
+  final open = '('.codeUnitAt(0);
+  final close = ')'.codeUnitAt(0);
 
-
-
-bool check_balanced(String s) {
-   List<String> stack = [];
-
-   for (int i = 0; i < s.length; i++) {
-      String c = s[i];
-
-      if (c == '(') {
-        stack.add(c);
-      } else if (c == ')') {
-        if (stack.isEmpty) {
+  for (int codeUnit in text.codeUnits) {
+    if (codeUnit == open) {
+      stack.add(codeUnit);
+    } else if (codeUnit == close) {
+      if (stack.isEmpty) {
+        return false;
+      } else {
+        if (stack.last == open) {
+          stack.removeLast();
+        } else {
           return false;
         }
-        stack.removeLast();
       }
-   }
-
-   return stack.isEmpty;
+    }
   }
+
+  return stack.isEmpty;
+}
+
